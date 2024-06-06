34fb56d00523a06dea8aedd7b12be0b162d908aa
Victorio Nikolaev <vikinik01@abv.bg>
2024-06-06 10:25:05 +0300

    chore(db): Changing Db generate script

--- apps/web/lib/consts.ts
+++ apps/web/lib/consts.ts
@@ -1,5 +1,7 @@
 export const __IS_DEV__ = process.env.NODE_ENV === "development";
 
+export const __IS_PROD__ = process.env.NODE_ENV === "production";
+
 export const HTTP = {
    MEDIA_TYPES: {
       APPLICATION_JSON: `application/json`,
